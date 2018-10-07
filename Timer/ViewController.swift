02fa4938576d801506171874b979d5bee8dc0505
Amber <amber_ak@live.com>
2018-10-07 20:38:53 +0300

    Timer in single view app

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,6 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var lbl: UILabel!
+    
+    var time = 0
+    
+    //timer
+    
+    var timer = Timer()
+    
+    
+    @IBAction func start(_ sender: UIButton)
+    {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true)
+    }
+    
+    
+    @IBAction func pause(_ sender: UIButton)
+    {
+        timer.invalidate()
+    }
+    
+    
+    @IBAction func reset(_ sender: UIButton)
+    {
+        timer.invalidate()
+        time = 0
+    }
+    
+    @objc func action ()
+    {
+        time += 1
+        lbl.text = String(time)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
